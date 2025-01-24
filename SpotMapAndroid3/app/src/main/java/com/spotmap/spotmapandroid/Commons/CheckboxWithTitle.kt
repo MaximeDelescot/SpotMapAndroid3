1547e2a5016aa3f278d2cf16cd66c2e5b9868796
Maxime DELESCOT <maxime.delescot@gmail.com>
2025-01-24 14:21:18 +0100

    create checkbox

---
+++ SpotMapAndroid3/app/src/main/java/com/spotmap/spotmapandroid/Commons/CheckboxWithTitle.kt
@@ -0,0 +1,52 @@
+package com.spotmap.spotmapandroid.Commons
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spotmap.spotmapandroid.R
+
+@Composable
+fun CheckboxWithTitle(title:String, isChecked: MutableState<Boolean>) {
+
+    Row(modifier = Modifier.height(40.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Checkbox(
+            colors = CheckboxDefaults.colors(
+                checkedColor = colorResource(id = R.color.PrimaryColor),
+                uncheckedColor = colorResource(id = R.color.LightColor),
+                checkmarkColor = colorResource(id = R.color.LightColor)
+            ),
+            checked = isChecked.value,
+            onCheckedChange = { isChecked.value = it }
+        )
+        Text(
+            text = title,
+            color = colorResource(id = R.color.LightColor)
+        )
+    }
+}
+@Preview(showBackground = false)
+@Composable
+fun PreviewCheckBox() {
+    var isChecked = remember { mutableStateOf(true) }
+    CheckboxWithTitle(title = "Need to pay to access", isChecked = isChecked )
+}
--- SpotMapAndroid3/app/src/main/java/com/spotmap/spotmapandroid/Screens/AddSpot/AddSpotScreen.kt
+++ SpotMapAndroid3/app/src/main/java/com/spotmap/spotmapandroid/Screens/AddSpot/AddSpotScreen.kt
@@ -2,13 +2,23 @@ package com.spotmap.spotmapandroid.Screens.AddSpot
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -16,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.spotmap.spotmapandroid.Commons.BasicSpacer
+import com.spotmap.spotmapandroid.Commons.CheckboxWithTitle
 import com.spotmap.spotmapandroid.Commons.CustomTextField
 import com.spotmap.spotmapandroid.Commons.GeneralButton
 import com.spotmap.spotmapandroid.Commons.SegmentedButton
@@ -28,12 +39,19 @@ fun AddSpotScreen(modifier: Modifier = Modifier) {
 
         Column(verticalArrangement = Arrangement.Top, modifier = modifier.padding(16.dp).background(color = colorResource(id = R.color.SecondaryColor), shape = RoundedCornerShape(16.dp)).padding(start = 16.dp, end = 16.dp, top = 32.dp, bottom = 32.dp)) {
             AddGeneralInformationView(modifier)
+
+
         }
         Spacer(modifier = modifier.weight(1f))
     }
 }
 @Composable
 fun AddGeneralInformationView(modifier: Modifier) {
+
+    var isPayableSelected = remember { mutableStateOf(false) }
+    var isDIYSelected = remember { mutableStateOf(false) }
+    var isWaterSelected = remember { mutableStateOf(false) }
+
     TitleText("Add general informations (1/3)")
     BasicSpacer()
     CustomTextField(placeholder = "Name", onTextDidChange = {})
@@ -42,12 +60,18 @@ fun AddGeneralInformationView(modifier: Modifier) {
     BasicSpacer()
     SegmentedButton(modifier = modifier.fillMaxWidth(), itemsTitles = listOf("STREET", "PARK"))
     BasicSpacer()
+    CheckboxWithTitle(title = "Need to pay access", isChecked = isPayableSelected)
+    BasicSpacer()
+    CheckboxWithTitle(title = "Is D.I.Y", isChecked = isDIYSelected)
+    BasicSpacer()
+    CheckboxWithTitle(title = "Has access to water", isChecked = isWaterSelected)
+    BasicSpacer()
     GeneralButton("Next", onClick = {})
-
 }
 
 @Composable
 fun TitleText(text: String) {
+
     Text(text,
         color = colorResource(id = R.color.LightColor),
         fontWeight = FontWeight.Bold,
